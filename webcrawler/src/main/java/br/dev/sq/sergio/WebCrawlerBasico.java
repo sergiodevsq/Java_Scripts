1fb9942461837bb3e203aa9f54fb1748b6083385
sergiodevsq <sergio@sq.dev.br>
2021-10-14 21:36:10 -0300

    Adicionando script para web crawling

---
+++ webcrawler/src/main/java/br/dev/sq/sergio/WebCrawlerBasico.java
@@ -0,0 +1,49 @@
+package br.dev.sq.sergio;
+
+import java.io.IOException;
+import java.util.HashSet;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class WebCrawlerBasico {
+	
+	private HashSet<String> links;
+
+    public WebCrawlerBasico() {
+        links = new HashSet<String>();
+    }
+	
+    public void getPageLinks(String URL) {
+        
+        if (!links.contains(URL)) {
+            try {
+                //4. (i) If not add it to the index
+                if (links.add(URL)) {
+                    System.out.println(URL);
+                }
+
+                //2. Fetch the HTML code
+                Document document = Jsoup.connect(URL).get();
+                //3. Parse the HTML to extract links to other URLs
+                Elements linksOnPage = document.select("a[href]");
+
+                //5. For each extracted URL... go back to Step 4.
+                for (Element page : linksOnPage) {
+                    getPageLinks(page.attr("abs:href"));
+                }
+            } catch (IOException e) {
+                System.err.println("For '" + URL + "': " + e.getMessage());
+            }
+        }
+    }
+	
+	
+	public static void main(String[] args) {
+		// 
+		new WebCrawlerBasico().getPageLinks("https://www.ibge.gov.br/");
+	}
+
+}
